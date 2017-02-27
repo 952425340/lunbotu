c6b6fd15652c13289c4a28e97aff16d817d0ff79
XBN <952425340@qq.com>
2017-02-27 11:07:20 +0800

    面向对象编程

--- 轮播图/js/jquery.zyslide.js
+++ 轮播图/js/jquery.zyslide.js
@@ -88,7 +88,7 @@ slide();*/
 $(function(){
 	alert('自运行的匿名函数');
 });*/
-(function($){
+/*(function($){
 	
 	// 本函数每次调用只负责一个轮播图的功能
 	// 也就是说只会产生一个轮播图,这个函数的作用域只能分配一个轮播图
@@ -171,7 +171,7 @@ $(function(){
 		// 支持链式调用
 		return this;
 	}
-})(jQuery);
+})(jQuery);*/
 
 /**
  * 用 jQuery 封装插件的几种写法:
@@ -198,3 +198,101 @@ $(function(){
  * 3.标签 class 的值得问题:prev、next.这里 class 太大众化了, 谁写标签都想叫 pre 或者 next,势必会冲突.
  * 4.插件文件名命名问题:index.js、index.css,命名大众化.比如这样修改:jQuery-ZYSlide.js
  */
+
+
+// 面向对象的轮播图插件的写法:
+(function($){
+	
+	// 构造函数
+	function Slide(ele,options){
+		// 转化为 jquery 对象
+		this.$ele = $(ele);
+		// 默认设置选项
+		this.setting = {
+			// 控制刚炸开的时间
+			delay:1000,
+			// 控制 interval 的时间 (轮播时间)
+			speed:2000
+		};
+		
+		// 对象合并
+		$.extend(true,this.setting, options);
+		
+		// 规定好每张图片处于的位置和状态
+		this.states = [
+						{ZIndex:1,width:120,height:150,top:69,left:134,ZOpacity:0.2},
+						{ZIndex:2,width:130,height:170,top:59,left:0,ZOpacity:0.5},
+						{ZIndex:3,width:170,height:218,top:35,left:110,ZOpacity:0.7},
+						{ZIndex:4,width:224,height:288,top:0,left:263,ZOpacity:1},
+						{ZIndex:3,width:170,height:218,top:35,left:470,ZOpacity:0.7},
+						{ZIndex:2,width:130,height:170,top:59,left:620,ZOpacity:0.5},
+						{ZIndex:1,width:120,height:150,top:69,left:500,ZOpacity:0.2}
+					 ];
+		this.lis = this.$ele.find('li');
+		this.interval = null;
+		
+		// 让 li 从正中间展开
+		this.move();
+		// 开启自动轮播
+		this.autoPlay();
+		
+
+		this.$ele.find('.zy-prev').click(function(){
+			this.prev();
+		}.bind(this));
+		// 点击下一张 (section)
+		this.$ele.find('.zy-next').click(function(){
+			this.next();
+		}.bind(this));
+		
+
+		// 停止轮播
+		this.$ele.find('section').add(this.lis).hover(function(){
+			clearInterval(this.interval);	
+		}.bind(this),function(){
+			this.autoPlay();
+		}.bind(this));
+	}
+	// 下一张，让轮播图发生偏移
+	Slide.prototype.move=function(){
+		this.lis.each(function(index,value){
+			// 注意这里的 this 指向问题. 这里的 this 指向某个 li
+			// 两个解决办法: 1.定义变量传进去   2.绑定
+			var state = this.states[index];
+			$(value).css("z-index",state.ZIndex).finish().animate(state,this.setting.delay).find('img').css('opacity',state.ZOpacity);
+		}.bind(this));
+	}
+	
+	// 下一张，让轮播图发生偏移
+	Slide.prototype.next=function(){
+		// 原理：把数组最后一个元素移到数组的第一位
+		//var obj = states.pop();
+		//states.unshift(obj);
+		this.states.unshift(this.states.pop());
+		this.move();
+	}
+	
+	Slide.prototype.prev=function(){
+		this.states.push(this.states.shift());
+		this.move();
+	}
+	
+	// 自动轮播
+	Slide.prototype.autoPlay=function(){
+		var _this = this;
+		this.interval=setInterval(function(){
+			// 注意这里的 this 指向 window
+			_this.next();
+		},this.setting.speed);
+	}
+	
+	// 找到要轮播的轮播图的根标签,调用 slide 方法
+	$.fn.zySlide = function(options){
+		this.each(function(index,value){
+			new Slide(value,options);
+			
+		});
+		// 支持链式调用
+		return this;
+	}
+})(jQuery);
